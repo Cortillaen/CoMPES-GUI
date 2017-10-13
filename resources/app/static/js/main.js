21f98d2ec180b37f93174c8bb6e3bc4c6800c858
Cortillaen <senkairyu@gmail.com>
2017-10-12 20:49:10 -0500

    Moved main.js to static/js/ and adjusted paths.

--- resources/app/static/js/main.js
+++ resources/app/static/js/main.js
@@ -11,7 +11,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   mainWindow.loadURL(url.format({
-  pathname: path.join(path.join(path.dirname(__dirname), 'templates'), 'index.html'),
+  pathname: path.join(path.join(path.dirname(path.dirname(__dirname)), 'templates'), 'index.html'),
   protocol: 'file:',
   slashes: true
 }))
