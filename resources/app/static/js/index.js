adfba62da59d23ccd8439d7f5008b33ee5d17d51
Trenton Nale <senkairyu@gmail.com>
2017-11-14 13:43:59 -0600

    Basic force-directed graph added

--- resources/app/static/js/index.js
+++ resources/app/static/js/index.js
@@ -63,6 +63,147 @@ function Viewmodel() {
 		this.chosen_algorithm = ko.observable(self.pe_algorithms()[0]);
 		this.hubs = ko.observableArray([]);
 	}
+	
+	function d3Data() {
+		/*
+		Author: Tim Roth
+		Contributors: Trenton Nale
+		Description: Force directed graph adapted from Tim Roth's example at
+					https://bl.ocks.org/puzzler10/4438752bb93f45dc5ad5214efaa12e4a
+		Input: N/A
+		Output: N/A
+		Notes: Builds a set of variables that depend on the Map View being active
+				changing to another screen without clearing these will throw errors
+		*/
+		var svg = d3.select("svg");
+		var width = +svg.attr("width");
+		var height = +svg.attr("height");
+		
+		var radius = 15;
+		
+		var nodes_data = [
+			{"name": "Lillian", "sex": "F"},
+			{"name": "Gordon", "sex": "M"}
+		];
+		var links_data = [
+			{"source": "Lillian", "target": "Gordon", "type":"A" }
+		];
+		
+		var simulation = d3.forceSimulation().nodes(nodes_data);
+		var link_force = d3.forceLink(links_data).id(function(d) {return d.name;});
+		var charge_force = d3.forceManyBody().strength(-100);
+		var center_force = d3.forceCenter(width / 2, height / 2);
+		simulation
+			.force("charge_force", charge_force)
+			.force("center_force", center_force)
+			.force("links", link_force)
+		;
+		
+		//add tick instructions: 
+		simulation.on("tick", tickActions );
+
+		//add encompassing group for the zoom 
+		var g = svg.append("g")
+			.attr("class", "everything");
+
+		//draw lines for the links 
+		var link = g.append("g")
+			  .attr("class", "links")
+			.selectAll("line")
+			.data(links_data)
+			.enter().append("line")
+			  .attr("stroke-width", 2)
+			  .style("stroke", linkColour);        
+
+		//draw circles for the nodes 
+		var node = g.append("g")
+				.attr("class", "nodes") 
+				.selectAll("circle")
+				.data(nodes_data)
+				.enter()
+				.append("circle")
+				.attr("r", radius)
+				.attr("fill", circleColour);
+		 
+		 
+		//add drag capabilities  
+		var drag_handler = d3.drag()
+			.on("start", drag_start)
+			.on("drag", drag_drag)
+			.on("end", drag_end);	
+			
+		drag_handler(node);
+
+
+		//add zoom capabilities 
+		var zoom_handler = d3.zoom()
+			.on("zoom", zoom_actions);
+
+		zoom_handler(svg);     
+		alert(nodes_data);
+
+		/** Functions **/
+
+		//Function to choose what color circle we have
+		//Let's return blue for males and red for females
+		function circleColour(d){
+			if(d.sex =="M"){
+				return "blue";
+			} else {
+				return "pink";
+			}
+		}
+
+		//Function to choose the line colour and thickness 
+		//If the link type is "A" return green 
+		//If the link type is "E" return red 
+		function linkColour(d){
+			if(d.type == "A"){
+				return "green";
+			} else {
+				return "red";
+			}
+		}
+
+		//Drag functions 
+		//d is the node 
+		function drag_start(d) {
+		 if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+			d.fx = d.x;
+			d.fy = d.y;
+		}
+
+		//make sure you can't drag the circle outside the box
+		function drag_drag(d) {
+		  d.fx = d3.event.x;
+		  d.fy = d3.event.y;
+		}
+
+		function drag_end(d) {
+		  if (!d3.event.active) simulation.alphaTarget(0);
+		  d.fx = null;
+		  d.fy = null;
+		}
+
+		//Zoom functions 
+		function zoom_actions(){
+			g.attr("transform", d3.event.transform)
+		}
+
+		function tickActions() {
+			//update circle positions each tick of the simulation 
+			node
+				.attr("cx", function(d) { return d.x; })
+				.attr("cy", function(d) { return d.y; });
+				
+			//update link positions 
+			link
+				.attr("x1", function(d) { return d.source.x; })
+				.attr("y1", function(d) { return d.source.y; })
+				.attr("x2", function(d) { return d.target.x; })
+				.attr("y2", function(d) { return d.target.y; });
+		} 
+	}
 
 	//============================= Data Bindings & Variables ===============================
 	var self = this;
@@ -108,6 +249,9 @@ function Viewmodel() {
 	//============================= Login Page Variables ====================================
 	self.user = ko.observable("");
 	self.pass = ko.observable("");
+	
+	//============================== Map View Variables =====================================
+	self.mapData = null;
 
 	//=================================General Functions=====================================
 	self.bonsai = function() {
@@ -230,6 +374,8 @@ function Viewmodel() {
 		*/
 		self.current_screen("map_screen");
 		self.bonsai();
+		self.setupMap();
+		alert(d3Data.width);
 	}
 
 	self.mapSidebarClick = function(clickedItem) {
@@ -244,6 +390,17 @@ function Viewmodel() {
 			alert("You clicked on " + clickedItem.network_ID() + " on the Map Screen.");
 		else
 			alert("You clicked on " + clickedItem.id() + " on the Map Screen.");
+	}
+	
+	self.setupMap = function() {
+		/*
+		Author: Trenton Nale
+		Description: Configure d3 to display network map
+		Input: N/A
+		Output: N/A
+		Notes: N/A
+		*/
+		self.mapData = new d3Data();
 	}
 
 	//-------------------------------- Informational View ------------------------
